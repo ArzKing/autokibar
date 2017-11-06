bb6f02c1a37a7e55eb71691cd2a28b752729507b
ArzKing <33385473+ArzKing@users.noreply.github.com>
2017-11-06 19:40:01 +0800

    Update kibar.py

--- kibar.py
+++ kibar.py
@@ -23,11 +23,23 @@ print client._loginresult()
 
 
 
-elif msg.text in ["Send oa","send oa","Kibar","kibar","SYNDROME ARMY IN ACTION~]:
-                if msg.from_ in staff:
+ if msg.text in ["Send oa","send oa","Kibar","kibar","SYNDROME ARMY IN ACTION~]:
+              if msg.from_ in staff:
                     msg.contentType = 13
                     msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
                     
+                    cl.sendMessage(msg)
+                    msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
+                    cl.sendMessage(msg)
+                    msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
+                    cl.sendMessage(msg)
+                    msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
+                    cl.sendMessage(msg)
+                    msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
+                    cl.sendMessage(msg)
+                    msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
+                    cl.sendMessage(msg)
+                    msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
                     cl.sendMessage(msg)
                     msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
                     cl.sendMessage(msg)
