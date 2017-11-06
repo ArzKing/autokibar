d14b4ea5d60cf0d9a585b09a74ec22c557b50a35
ArzKing <33385473+ArzKing@users.noreply.github.com>
2017-11-06 19:22:28 +0800

    Create kibar.py

---
+++ kibar.py
@@ -0,0 +1,18 @@
+elif msg.text in ["Send oa","send oa","Kibar","kibar","SYNDROME ARMY IN ACTION~]:
+                if msg.from_ in staff:
+                    msg.contentType = 13
+                    msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
+                    
+                    cl.sendMessage(msg)
+                    msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
+                    cl.sendMessage(msg)
+                    msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
+                    cl.sendMessage(msg)
+                    msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
+                    cl.sendMessage(msg)
+                    msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
+                    cl.sendMessage(msg)
+                    msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
+                    cl.sendMessage(msg)
+                    msg.contentMetadata = {'mid': 'u11bf06d9ce76ccc78b7b30d06a20767b'}
+                    cl.sendMessage(msg)
