f7ba4ddfcc54f9f1fa20ac862446431c3f8bd327
ArzKing <33385473+ArzKing@users.noreply.github.com>
2017-11-06 19:35:04 +0800

    Update kibar.py

--- kibar.py
+++ kibar.py
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
+# Edited from script LineVodka script made by Merkremont
+
+from LineAlpha import LineClient
+from LineAlpha.LineApi import LineTracer
+from LineAlpha.LineThrift.ttypes import Message
+from LineAlpha.LineThrift.TalkService import Client
+import time, datetime, random ,sys, re, string, os, json
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+client = LineClient()
+client._qrLogin("line://au/q/")
+
+
+profile, setting, tracer = client.getProfile(), client.getSettings(), LineTracer(client)
+offbot, messageReq, wordsArray, waitingAnswer = [], {}, {}, {}
+
+
+
+print client._loginresult()
+
+
+
 elif msg.text in ["Send oa","send oa","Kibar","kibar","SYNDROME ARMY IN ACTION~]:
                 if msg.from_ in staff:
                     msg.contentType = 13
